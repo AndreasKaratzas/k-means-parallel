7568b049c27b331fd1a433c3639e0c059b34cf72
Andreas <42451412+andreasceid@users.noreply.github.com>
2020-12-03 16:24:14 +0200

    Update Validation.cpp

--- k-means-parallel/Validation.cpp
+++ k-means-parallel/Validation.cpp
@@ -13,17 +13,17 @@ static std::vector<std::pair<int, long double>> kmeans_progress;             ///
  */
 void export_multidimentional_float_vector(const std::vector<std::array<float, Nv>> Obj, std::string filename)
 {
-	std::ofstream export_stream;                                               ///< Defines an output file stream
-	export_stream.open("./data/" + filename + ".csv");                         ///< Associates `export_stream` with a CSV file named after the `filename` variable
-	for (int i = 0; i < Obj.size(); i += 1)                                    ///< Loops through `Obj`
+	std::ofstream export_stream;                                                           ///< Defines an output file stream
+	export_stream.open("./data/" + filename + ".csv");                                     ///< Associates `export_stream` with a CSV file named after the `filename` variable
+	for (int i = 0; i < Obj.size(); i += 1)                                                ///< Loops through `Obj`
 	{
-		for (int j = 0; j < Obj.at(i).size(); j += 1)                            ///< Loops through the elements of the array inside `Obj` that is being parsed
+		for (int j = 0; j < Obj.at(i).size(); j += 1)                                  ///< Loops through the elements of the array inside `Obj` that is being parsed
 		{
 			export_stream << Obj.at(i)[j] << (j < Obj.at(i).size() - 1 ? "," : "");///< Exports element of that array to the `export_stream` file stream
 		}
 		export_stream << std::endl;
 	}
-	export_stream.close();                                                     ///< Closes file stream
+	export_stream.close();                                                                 ///< Closes file stream
 }
 
 /**
@@ -36,17 +36,17 @@ void export_multidimentional_float_vector(const std::vector<std::array<float, Nv
  */
 void export_multidimentional_integer_array(const std::array<std::vector<int>, Nc> Obj, std::string filename)
 {
-	std::ofstream export_stream;                                               ///< Defines an output file stream
-	export_stream.open("./data/" + filename + ".csv");                         ///< Associates `export_stream` with a CSV file named after the `filename` variable
-	for (int i = 0; i < Obj.size(); i += 1)                                    ///< Loops through `Obj`
+	std::ofstream export_stream;                                                           ///< Defines an output file stream
+	export_stream.open("./data/" + filename + ".csv");                                     ///< Associates `export_stream` with a CSV file named after the `filename` variable
+	for (int i = 0; i < Obj.size(); i += 1)                                                ///< Loops through `Obj`
 	{
-		for (int j = 0; j < Obj[i].size(); j += 1)                               ///< Loops through the elements of the vector inside `Obj` that is being parsed
+		for (int j = 0; j < Obj[i].size(); j += 1)                                     ///< Loops through the elements of the vector inside `Obj` that is being parsed
 		{
 			export_stream << Obj[i].at(j) << (j < Obj.at(i).size() - 1 ? "," : "");///< Exports element of that vector to the `export_stream` file stream
 		}
 		export_stream << std::endl;
 	}
-	export_stream.close();                                                     ///< Closes file stream
+	export_stream.close();                                                                 ///< Closes file stream
 }
 
 /**
@@ -67,13 +67,13 @@ void track_kmeans_progress(int iter_counter, long double norm_iter_conv)
  */
 void export_kmeans_progress(std::string filename)
 {
-	std::ofstream export_stream;                                               ///< Defines an output file stream
-	export_stream.open("./data/" + filename + ".txt");                         ///< Associates `export_stream` with a CSV file named after the `filename` variable
-	for (int i = 0; i < kmeans_progress.size(); i += 1)                        ///< Loops through `kmeans_progress`
+	std::ofstream export_stream;                                                            ///< Defines an output file stream
+	export_stream.open("./data/" + filename + ".txt");                                      ///< Associates `export_stream` with a CSV file named after the `filename` variable
+	for (int i = 0; i < kmeans_progress.size(); i += 1)                                     ///< Loops through `kmeans_progress`
 	{
 		export_stream <<
 			"Iteration: " << kmeans_progress.at(i).first <<
-			"Convergence: " << kmeans_progress.at(i).second << std::endl;          ///< Exports an element of `kmeans_progress` vector to the `export_stream` file stream
+			"Convergence: " << kmeans_progress.at(i).second << std::endl;           ///< Exports an element of `kmeans_progress` vector to the `export_stream` file stream
 	}
-	export_stream.close();                                                     ///< Closes file stream
+	export_stream.close();                                                                  ///< Closes file stream
 }
