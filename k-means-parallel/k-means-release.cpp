6a0962b8c2a25be545bb4ad82bebc039e57313f2
Andreas <42451412+andreasceid@users.noreply.github.com>
2020-12-03 16:28:00 +0200

    Update k-means-release.cpp

--- k-means-parallel/k-means-release.cpp
+++ k-means-parallel/k-means-release.cpp
@@ -21,31 +21,31 @@ int main(void)
 	{
 #pragma omp master
 		{
-			Vec.reserve(N);                                                                           ///< Reserves `N` blocks of memory to increase speed
-			vec_init(Vec);                                                                            ///< Fills `Vec` with random real numbers
+			Vec.reserve(N);                                                               ///< Reserves `N` blocks of memory to increase speed
+			vec_init(Vec);                                                                ///< Fills `Vec` with random real numbers
 		}
 #pragma omp single
 		{
-			old_Center.reserve(Nc);                                                                   ///< Reserves `Nc` blocks of memory to increase speed
-			init_centers(old_Center);                                                                 ///< Fills `old_Center` with random vectors from `Vec`
+			old_Center.reserve(Nc);                                                       ///< Reserves `Nc` blocks of memory to increase speed
+			init_centers(old_Center);                                                     ///< Fills `old_Center` with random vectors from `Vec`
 		}
 	}
 	std::chrono::time_point<std::chrono::system_clock> optimization_benchmark = init_benchmark(); ///< K-means optimization loop benchmark 
 	for (int iter_counter = 1; norm_iter_conv > THR_KMEANS; iter_counter += 1)                    ///< K-means optimazation loop
 	{
 		std::chrono::duration<double> bench_res =
-			bench_loop(prev_loop_benchmark, loop_benchmark = init_benchmark());                       ///< Benchmarks loop
-		kmeans_progress(iter_counter, iter_conv, norm_iter_conv, bench_res, VERBOSITY);             ///< Outputs K-means progress
-		compute_classes(Vec, old_Center, Classes);                                                  ///< Re-computes `Classes` contents
-		optimize_center(Vec, new_Center, Classes);                                                  ///< Computes error of current clusters and stores it to the variable `new_Center`
-		long double prev_iter_conv = iter_conv;                                                     ///< Stores previous `iter_conv` to use it to normalize convergence
-		iter_conv = convergence(new_Center, old_Center);                                            ///< Computes convergence of current K-means loop
-		norm_iter_conv = normalize_convergence(iter_conv, prev_iter_conv, iter_counter);            ///< Normalizes `iter_conv` to avoid overfitting
-		old_Center.swap(new_Center);                                                                ///< Swaps `old_Center` with `new_Center` to start the next loop
+			bench_loop(prev_loop_benchmark, loop_benchmark = init_benchmark());           ///< Benchmarks loop
+		kmeans_progress(iter_counter, iter_conv, norm_iter_conv, bench_res, VERBOSITY);       ///< Outputs K-means progress
+		compute_classes(Vec, old_Center, Classes);                                            ///< Re-computes `Classes` contents
+		optimize_center(Vec, new_Center, Classes);                                            ///< Computes error of current clusters and stores it to the variable `new_Center`
+		long double prev_iter_conv = iter_conv;                                               ///< Stores previous `iter_conv` to use it to normalize convergence
+		iter_conv = convergence(new_Center, old_Center);                                      ///< Computes convergence of current K-means loop
+		norm_iter_conv = normalize_convergence(iter_conv, prev_iter_conv, iter_counter);      ///< Normalizes `iter_conv` to avoid overfitting
+		old_Center.swap(new_Center);                                                          ///< Swaps `old_Center` with `new_Center` to start the next loop
 	}
 	std::chrono::time_point<std::chrono::system_clock> end = terminate_bench();                   ///< Initializes the endpoint of K-means benchmark
 	std::pair<std::chrono::duration<double>, std::chrono::duration<double>> bench_res =
-		benchmark_results(start, optimization_benchmark, end);                                      ///< Stores benchmark results
+		benchmark_results(start, optimization_benchmark, end);                                ///< Stores benchmark results
 	kmeans_termination(bench_res, norm_iter_conv);                                                ///< Outputs benchmark results
 	return 0;
 }
